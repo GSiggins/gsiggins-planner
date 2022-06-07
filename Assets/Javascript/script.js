8a1bbb94e0967e2d08c3feb83771c9dbfab86ce4
GSiggins <gregory.siggins@gmail.com>
2022-06-06 23:39:08 -0700

    started adding javascript to handle localstorage and event listeners

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -12,30 +12,25 @@ console.log(timeArray)
 
 
 function checkTime(timeArray) {
-    let currentHour = (moment().format('h'));
-    console.log(currentHour);
+    let currentHour = (moment().format('12'));
+    console.log("the current hour is " + currentHour);
     for (let i = 0; i < timeArray.length; i++) {
-        let index = timeArray[i]
-        if (currentHour == index.dataset.num) {
-            console.log(i);
-            console.log(index.dataset.num);
+        let hourCheck = parseInt(timeArray[i].dataset.num)
+        if (currentHour == hourCheck) {
+            console.log("This is the current index I am checking: " + i);
+            console.log(hourCheck + " is the current hour");
             timeArray[i].className = "present col-sm-9";
-        } else if (currentHour > index.dataset.num) {
+        } else if (currentHour > hourCheck) {
             timeArray[i].className = "past col-sm-9";
-            console.log(index.dataset.num);
-            console.log(i);
+            console.log(hourCheck + " is in the past.");
+            console.log("This is the current index I am checking: " + i);
         } else {
             timeArray[i].className = "future col-sm-9";
-            console.log(index.dataset.num);
-            console.log(i);
+            console.log("It is not yet " + hourCheck);
+            console.log("This is the current index I am checking: " + i);
         }
     }
 }
-
-
-
-
-
 
 let timeclock = function () {
     var time = moment().format('[It is currently ] dddd, MMMM Do YYYY [ at ] hh:mm:ss a');
@@ -44,4 +39,18 @@ let timeclock = function () {
 
 timeclock()
 setInterval(timeclock, 1000)
-checkTime(timeArray)
+checkTime(timeArray, 150000)
+
+function storeText (event) {
+    console.log("storetext init")
+var target = $(event.target)
+if (target.is('saveBtn')) {
+    var userText = target.siblings('user-text').val().trim();
+    localStorage.setItem('savedText', JSON.stringify(userText));
+}
+}
+
+
+
+
+$('.saveBtn').on('click', storeText)
