489d8f956c8828ff77cf6d3b18d36b0caa9a2186
GSiggins <gregory.siggins@gmail.com>
2022-06-06 18:29:30 -0700

    started with basic structuring of planner/hours, started javascript

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,6 +1,30 @@
+var nineAM = $('9')
+var tenAM = $('10')
+var elevenAM = $('11')
+var twelvePM = $('12')
+var onePM = $('13')
+var twoPM = $('14')
+var threePM = $('15')
+var fourPM = $('16')
+var fivePM = $('17')
+var timeArray = [nineAM, tenAM, elevenAM, twelvePM, onePM, twoPM, threePM, fourPM, fivePM]
+
+function checkTime() {
+    let currentHour = moment().format('H').value
+    for (let i = 0; i < timeArray.length; i++) {
+        if (currentHour == timeArray.)
+        
+    }
+    if (currentHour === 
+}
+
+
+
+
+
 
 let timeclock = function() {
-    var time = moment().format('[It is currently ] dddd MM Do YYYY [ at ] hh:mm:ss a');
+    var time = moment().format('[It is currently ] dddd, MMMM Do YYYY [ at ] hh:mm:ss a');
     document.querySelector('#currentDay').innerHTML = time;
 }
 
